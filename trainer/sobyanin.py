9975628d6878f226266ff56b620a000e08e77a07
komunre <koliziy5@gmail.com>
2022-04-28 15:29:35 +0700

    BETTER!

--- trainer/sobyanin.py
+++ trainer/sobyanin.py
@@ -8,19 +8,23 @@ import torch
 from mytokenizer import WordsDataset
 import mytokenizer
 import requests
+from os.path import exists
 
+dataloader = DataLoader(WordsDataset(wordgen2.data, 2), batch_size=wordgen2.batch_size)
 
-model = wordgen2.NeuralNewsGen(wordgen2.train_dataloader).to(device=wordgen2.device)
-model.load_state_dict(torch.load("model.pth"))
+print("Vocab size: " + str(len(dataloader.dataset)))
 
-train_dataloader = wordgen2.train_dataloader
-
-print("Vocab size: " + str(len(train_dataloader.dataset)))
+model = wordgen2.NeuralNewsGen(dataloader).to(device=wordgen2.device)
+if exists("model_twolen.pth"):
+    model.load_state_dict(torch.load("model_twolen.pth"))
+else:
+    wordgen2.train(dataloader, model, wordgen2.loss_fn, wordgen2.optimizer, 12, 2)
+    torch.save(model.state_dict(), "model_twolen.pth")
 
 model.eval()
 
 while True:
-    announce = ' '.join(wordgen2.predict(wordgen2.train_dataloader, model, "Собянин заявил об огромном", 3))
+    announce = ' '.join(wordgen2.predict(wordgen2.train_dataloader, model, "Сергей Собянин", 7, 2))
     print(announce)
     token = open("secrets/token.txt").read()
     r = requests.post('http://informasoft.ru/NeuralNews/Create', data={'token': token, 'news': announce.encode(encoding='UTF-8',errors='strict')})
